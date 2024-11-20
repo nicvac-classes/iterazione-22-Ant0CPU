c57ea423fae13de785c462e81bbbc44fa044c23e
Ant0CPU <antonio.bruno@marconibari.edu.it>
2024-11-20 22:06:07 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,18 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+   int  f, n, m;
+
+   for (f=1; f<=10; f++)
+   {
+        cout<<"In output la tabellina del " <<f<<endl;
+
+        for (n=1; n<=10; n++)
+        {
+            m=f*n;
+            cout<<m<<endl;
+        }
+   }
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
